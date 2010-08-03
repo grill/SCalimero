a6524bab2f8bac183cade3b274b6243d09f45e3e
Alexander Steiner <alex@Zebes.(none)>
2010-08-03 11:37:50 +0200

    ...

--- src/test/scala/EventSpec.scala
+++ src/test/scala/EventSpec.scala
@@ -2,25 +2,24 @@ package test.scala
 
 import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
-import scala.collection.mutable.Stack
 import main.scala.util._
 
 class EventHelperSpec extends Spec with ShouldMatchers {
 
   describe("An EventHelper") {
 
-	  class EventHelperTest extends EventHelper(){
-	 	  
-	  }
-      val evhelper = new EventHelperTest()
+    class EventHelperTest extends EventHelper(){
 
-      it("should be able to add events") {
-    	  
-      }
+    }
+    val evhelper = new EventHelperTest()
 
-      it("should be able to remove events") {
-    	  
-      }
+    it("should be able to add events") {
+      
+    }
+
+    it("should be able to remove events") {
+
     }
   }
 }
+
