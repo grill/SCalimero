6b89284e13b78633108613ea7b81f907bd25c942
Alexander Steiner <alex@Zebes.(none)>
2010-08-03 13:52:24 +0200

    create, subscribe and call back events

--- src/main/scala/scalimero/util/EventHelper.scala
+++ src/main/scala/scalimero/util/EventHelper.scala
@@ -2,7 +2,23 @@ package scalimero.util
 
 import scala.collection.immutable.Map
 
-trait EventHelper[T] {
-  var callbacks = Map[Any, List[() => Unit]]()
+abstract trait EventHelper[T] {
+  val events : List[(T => Boolean, Any)]
+  var callbacks = Map[Any, List[() => Unit]]() withDefaultValue Nil
   
+  def eventList(in : T) =
+    events filter {_._1(in)} map {_._2}
+  
+  def subscribe(event : Any)(callback : => Unit) {
+    if (events forall {_._2 != event})
+      throw new NoSuchEventException("No event " + event)
+    
+    callbacks = callbacks updated (event, callback _ :: callbacks(event))
+  }
+  
+  def callEvents(value : T){
+    events.map((eventSig) => if(eventSig._1(value)) callbacks(eventSig._2).map(_()))
+  }
 }
+
+class NoSuchEventException(msg : String) extends Exception(msg)
--- src/test/scala/scalimero/util/EventSpec.scala
+++ src/test/scala/scalimero/util/EventSpec.scala
@@ -2,23 +2,40 @@ package scalimero.util.test
 
 import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
-import main.scala.util._
+import scalimero.util._
 
 class EventHelperSpec extends Spec with ShouldMatchers {
 
   describe("An EventHelper") {
-
-    class EventHelperTest extends EventHelper(){
-
-    }
-    val evhelper = new EventHelperTest()
-
-    it("should be able to add events") {
+    describe("(Hello World event)") {
+      var eh = new EventHelper[String] {
+          val events = List(
+            ({s:String => s == "hello"} -> "world")
+          )
+        }
       
-    }
-
-    it("should be able to remove events") {
-
+      it ("should give an empty event list when given \"foo\"") {
+        eh eventList "foo" should equal (Nil)
+      }
+      
+      it ("should give the \"world\" event when given \"hello\"") {
+        eh eventList "hello" should equal (List("world"))
+      }
+      
+      var worldCalled = false
+      
+      it ("should allow subscriptions to the \"world\" event") {
+        eh.subscribe("world"){worldCalled = true}
+      }
+      
+      it ("should not allow subscriptions to the \"foo\" event") {
+        evaluating {eh.subscribe("foo"){1+1}} should produce [NoSuchEventException]
+      }
+      
+      it ("should call back properly") {
+        eh callEvents "hello"
+        worldCalled should be (true)
+      }
     }
   }
 }
