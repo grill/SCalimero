4a8aa08f624e59dc975639028486f1e99b8da386
Alexander Steiner <alex@Zebes.(none)>
2010-08-04 10:06:46 +0200

    added EventHelper test

--- src/test/scala/org/scalimero/util/test/EventHelperSpec.scala
+++ src/test/scala/org/scalimero/util/test/EventHelperSpec.scala
@@ -79,6 +79,58 @@ class EventHelperSpec extends Spec with ShouldMatchers {
         worldCalled should be (0)
       }
     }
+    
+    describe("with multiple callbacks and events") {
+      it ("should work") {
+        val eh = new EventHelper[Int] {
+          var oldVal = 0
+          val events = List(
+            ({i:Int => i >= 10} -> ">=10"),
+            ({i:Int => i == 10} -> "=10"),
+            ({i:Int => i > 10} -> ">10"),
+            ({i:Int => i < 10} -> "<10"),
+            ({i:Int => val ret = i < oldVal; oldVal = i; ret} -> "<")
+          )
+        }
+        var ge10 = false
+        var ge102 = false
+        var e10 = false
+        var g10 = false
+        var s10 = false
+        var s = false
+        eh.subscribe(">=10"){ge10 = true}
+        val cb = eh.subscribe(">=10"){ge102 = true}
+        eh.subscribe("=10"){e10 = true}
+        eh.subscribe(">10"){g10 = true}
+        eh.subscribe("<10"){s10 = true}
+        eh.subscribe("<"){s = true}
+        
+        eh callEvents 15
+        
+        ge10 should be (true)
+        ge102 should be (true)
+        e10 should be (false)
+        g10 should be (true)
+        s10 should be (false)
+        
+        ge10 = false
+        ge102 = false
+        e10 = false
+        g10 = false
+        s10 = false
+        s = false
+        
+        cb.detach
+        eh callEvents 10
+        
+        ge10 should be (true)
+        ge102 should be (false)
+        e10 should be (true)
+        g10 should be (false)
+        s10 should be (false)
+        s should be (true)
+      }
+    }
   }
 }
 
