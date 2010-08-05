917df685d05e1003ac9d498568a08eb80b15b586
Alexander Steiner <alex@Zebes.(none)>
2010-08-05 10:45:25 +0200

    1st network draft

--- src/main/scala/org/scalimero/dsl/Network.scala
+++ src/main/scala/org/scalimero/dsl/Network.scala
@@ -1,5 +1,47 @@
 package org.scalimero.dsl
 
-object Network {
+import tuwien.auto.calimero.link._
+import tuwien.auto.calimero.link.medium._
+import tuwien.auto.calimero.process._
+import tuwien.auto.calimero._
 
+object Network {
+  var default : Network = null
+  var defaultMedium = TPSettings.TP1
+  
+  def apply(router : String) = {
+    default = new Network(router)
+    default
+  }
+}
+
+class Network(var router : String) {
+  var medium = Network.defaultMedium
+  var nl : KNXNetworkLink = null
+  var opened = false
+  var pc : ProcessCommunicator = null
+  
+  object pl extends ProcessListener{
+    def detached(e : DetachEvent) {}
+    def groupWrite(e : ProcessEvent) {}
+  }
+  
+  def open {
+    opened = true
+    pc = new ProcessCommunicatorImpl(networkLink)
+    pc.addProcessListener(pl)
+  }
+  
+  def close {
+    opened = false
+    if(nl != null && nl.isOpen)
+      nl.close
+  }
+  
+  def send(dp : Datapoint, value : String) = pc.write(dp, value)
+  def read(dp : Datapoint) = pc.read(dp)
+  
+  def networkLink = if(nl != null && nl.isOpen) nl else {
+    new KNXNetworkLinkIP(router, medium)
+  }
 }
