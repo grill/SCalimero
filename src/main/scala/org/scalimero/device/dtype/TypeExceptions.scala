6bfbe0ebee22dd535c71ec37bdd2d4860f609059
grill <gabriel.grill@aon.at>
2010-08-09 10:04:18 +0200

    Signed-off-by: grill <gabriel.grill@aon.at>

--- src/main/scala/org/scalimero/device/dtype/TypeExceptions.scala
+++
@@ -1,6 +0,0 @@
-package org.scalimero.device.dtype
-
-class OutOfBoundsException(value: String, min: String, max: String) extends Exception("The value = " + value +
-	"is out of bounds! Please choose a values from " + min + " to " + max + ".")
-	
-class TranslatorTypeNotFoundException(msg: String) extends Exception("The value " + msg + "is not a vaild mainNumber!")
--- src/main/scala/org/scalimero/device/dtype/package.scala
+++
@@ -1,8 +0,0 @@
-package org.scalimero.device
-
-package object dtype {
-  implicit def true2bool(t: True.type) = true
-  implicit def false2bool(t: False.type) = false
-  implicit def true2bool(t: True) = true
-  implicit def false2bool(t: False) = false
-}
