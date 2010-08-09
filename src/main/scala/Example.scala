49c713195bac30650ccf1bd573f2934e6e9062f6
Alexander Steiner <alex@Zebes.(none)>
2010-08-09 10:26:53 +0200

    wrote example, added open/close to network companion object

---
+++ src/main/scala/Example.scala
@@ -0,0 +1,25 @@
+import org.scalimero.dsl._
+import org.scalimero.device._
+import org.scalimero.device.preconf._
+import org.scalimero.device.dtype._
+import org.scalimero.device.dvalue._
+
+object Example extends Application {
+
+//the knx router towards our network is at 172.19.0.7, and it should immediately be opened
+Network("172.19.0.7") open
+
+//define our lamps
+val lA = Lamp("1/1/0")
+val lB = Lamp("1/1/1")
+
+//turn lamp A on and off
+lA turn on
+lA turn off
+
+//subscribe to lamp A's on event, i.e. lB gets turned on whenever lA is turned on
+lA.subscribe(on){
+  lB turn on
+}
+
+}
