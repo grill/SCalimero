e6cf3a47e0e8486955c49fb951458e53e5679b4d
grill <gabriel.grill@aon.at>
2010-08-10 13:05:52 +0200

    many files of package dvalue were deleted Signed-off-by: grill <gabriel.grill@aon.at>

--- src/main/scala/org/scalimero/device/dvalue/Date.scala
+++
@@ -1,7 +0,0 @@
-package org.scalimero.device.dvalue
-
-import java.text.SimpleDateFormat
-
-//Not tested
-class DATE(val value: SimpleDateFormat) extends DPValue
-
--- src/main/scala/org/scalimero/device/dvalue/String.scala
+++
@@ -1,7 +0,0 @@
-package org.scalimero.device.dvalue
-
-import java.text.SimpleDateFormat
-
-//Not tested
-class STRING(val value: SimpleDateFormat) extends DPValue
-
