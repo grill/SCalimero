ef9597cbccbb9f8953e97f6305b96fc5cc6d46d9
Alexander Steiner <alex_steiner@gmx.net>
2010-08-25 09:59:05 +0200

    implicit def dpvalue2native

--- src/main/scala/org/scalimero/device/dtype/package.scala
+++ src/main/scala/org/scalimero/device/dtype/package.scala
@@ -1,7 +1,9 @@
 package org.scalimero.device.dtype {
   trait implicits extends Num2ByteFloat.implicits with DateTime.implicits
     with Num2ByteUnsigned.implicits with Num4ByteUnsigned.implicits
-    with Num8BitUnsigned.implicits with String.implicits with Boolean.implicits
+    with Num8BitUnsigned.implicits with String.implicits with Boolean.implicits {
+    implicit def dptype2native[T](dpv : DPValue[T]) = dpv.value
+  }
 }
 
 package org.scalimero.device {
