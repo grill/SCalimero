23e0eeeb334da3becc38c701715d62d5b4bfc6b9
Alexander Steiner <alex_steiner@gmx.net>
2010-08-25 10:26:20 +0200

    initial commit

--- import-file.scala
+++
@@ -1,6 +0,0 @@
-import org.scalimero.device._
-import org.scalimero.device.dtype._
-import org.scalimero.device.dtype.boolean._
-import org.scalimero.dsl._
-import org.scalimero.device.preconf._
-import org.scalimero.device.dtype.translatortype._
