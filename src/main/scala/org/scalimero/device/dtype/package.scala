8c285b4459e752ff6940761e9b0166cc0ed2be89
grill <gabriel.grill@aon.at>
2010-08-25 15:16:49 +0200

    commit for the deleted files Signed-off-by: grill <gabriel.grill@aon.at>

--- src/main/scala/org/scalimero/device/dtype/package.scala
+++
@@ -1,9 +0,0 @@
-package org.scalimero.device.dtype {
-  trait implicits extends Num2ByteFloat.implicits with DateTime.implicits
-    with Num2ByteUnsigned.implicits with Num4ByteUnsigned.implicits
-    with Num8BitUnsigned.implicits with String.implicits with Boolean.implicits
-}
-
-package org.scalimero.device {
-  package object dtype extends dtype.implicits
-}
